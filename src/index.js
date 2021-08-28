5a5c7edb2daebf7d1536acf2ec633753cb31866d
Erica Rojo <ericarojo692@yahoo.com>
2021-08-28 13:53:17 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      buildingToDo(e.target["new-task-description"].value);
+      document.querySelector('form').reset()
+    });
+  });
+  
+  function buildingToDo(todo) {
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.textContent = "x";
+    li.style.color = "red"
+    li.textContent = `${todo} `;
+    li.appendChild(button);
+    button.addEventListener("click", handleDetele);
+    document.querySelector("#list").appendChild(li);
+    document.querySelector('form').reset();
+  }
+  
+  function handleDetele(e) {
+    e.target.parentNode.remove();
+  }
